2c2cda8c81c6a89ef7cc6c3aec2cfcffc24a9ba9
Brax Wong <ghostenjob@gmail.com>
2023-07-29 23:52:05 +0100

    fix: createAccount() function can now accept parameter by using lamdba function for the CTkbutton object.

--- src/new_account.py
+++ src/new_account.py
@@ -2,7 +2,6 @@ import customtkinter
 
 class NewAccount(customtkinter.CTkToplevel):
 
-    
     def initialiseWidget(self):
         self.grid_columnconfigure(0, weight = 1)
         self.grid_rowconfigure((0,1), weight = 1)
@@ -15,16 +14,16 @@ class NewAccount(customtkinter.CTkToplevel):
         username.grid(row = 1, column = 0, padx = 20, pady = 20)
         password = customtkinter.CTkTextbox(master = self, height = 20, width = 150)
         password.grid(row = 2, column = 0, padx = 20, pady = 20)
-        parameter = customtkinter.StringVar(value = username.get("0.0","end"))
-        submit = customtkinter.CTkSegmentedButton(self, values = ["Create Account"], command = self.createAccount, variable = parameter) 
+        submit = customtkinter.CTkButton(self, text = "Create Accout", command = lambda: self.createAccount(username.get("0.0","end"),password.get("0.0","end"))) 
         submit.grid(row = 3, column = 0, padx = 20, pady = 20)
 
     def __init__(self):
         super().__init__()
         self.initialiseWidget()
 
-    def createAccount(value, value2):
-        print(value2)
+    def createAccount(self, username, password):
+        print(username)
+        print(password)
 
     def accountExists():
         return True
